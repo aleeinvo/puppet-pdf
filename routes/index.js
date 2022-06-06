45c89d8508918dd93d9368aecaa5ed01bb91050b
Alee Dhillon <ali.asghar@invozone.com>
2022-06-06 17:32:31 +0500

    single page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 const puppeteer = require('puppeteer')
 
@@ -31,13 +32,41 @@ router.get('/', function(req, res, next) {
 router.get('/dev', async (req, res) => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('http://127.0.0.1:3000', {waitUntil: 'networkidle0'});
-  const pdf = await page.pdf({ format: 'A4', scale: 0.5 });
+
+  let user = {
+    name: 'Amna Mustafa',
+    skills: [
+      {
+        name: 'PHP',
+        level: 3
+      },
+      {
+        name: 'Node',
+        level: 5,
+      },
+      {
+        name: 'Laravel',
+        level: 2
+      },
+      {
+        name: 'Git',
+        level: 1
+      }
+    ]
+  };
+  res.render('index', { title: 'Express', user }, async (error, html) => {
+    await page.setContent(html);
+    // page.addStyleTag({
+    //   path: path.resolve('./public/css/bootstrap.css')
+    // });
+
+    const pdf = await page.pdf({ format: 'A4', scale: 0.5 });
  
   await browser.close();
   
   res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
 	res.send(pdf)
+  });
 })
 
 module.exports = router;
